20a1e424fde9ec7c30e03b21388f5d09f7f30f07
coretesu <info@docotekne.com>
2025-09-17 18:59:37 -0500

    alpha0.010

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Fragmentos';
+export const SITE_DESCRIPTION = 'Fragmentos Traducidos al Espanol Escritos por David Graeber'
